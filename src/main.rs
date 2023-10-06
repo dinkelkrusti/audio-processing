9307d5e0589006b5c4153b73d2d947eda7015a5a
ERIK_WITTEMEIER <erik.wittemeier@gmail.com>
2023-10-07 00:12:27 +0200

    feat: draw r o t u n d fella

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,42 @@
 use std::time::Duration;
 
+use bevy::prelude::*;
+use bevy::sprite::MaterialMesh2dBundle;
 use cpal::InputCallbackInfo;
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 
-fn main() -> anyhow::Result<()> {
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, circle_boi)
+        // .add_systems(Startup, listen_to_audio_in)
+        .run()
+}
+
+fn circle_boi(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>) {
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes.add(shape::Circle::new(50.).into()).into(),
+        material: materials.add(ColorMaterial::from(Color::PURPLE)),
+        transform: Transform::from_translation(Vec3::new(-150., 0., 0.)),
+        ..default()
+    });
+}
+
+fn listen_to_audio_in() {
+    fn err_fn(_: cpal::StreamError) {
+        println!("REEEEEEE")
+    }
+
     let host = cpal::default_host();
 
     let input_device = host.default_input_device()
         .expect("Failed to find input");
-    println!("Using input device: \"{}\"", input_device.name()?);
+    println!("Using input device: \"{}\"", input_device.name().unwrap());
 
 
-    let config: cpal::StreamConfig = input_device.default_input_config()?.into();
+    let config: cpal::StreamConfig = input_device.default_input_config().unwrap().into();
 
     let write_into_buffer = move |data: &[f32], _: &InputCallbackInfo| for sample in data {
         println!("{:?}", &sample);
@@ -22,15 +47,10 @@ fn main() -> anyhow::Result<()> {
         write_into_buffer,
         err_fn,
         None,
-    )?;
+    ).unwrap();
 
-    stream.play()?;
-    std::thread::sleep(Duration::from_secs(100));
+    stream.play().unwrap();
+
+    std::thread::sleep(Duration::from_secs(1));
     drop(stream);
-
-    return Ok(());
-}
-
-fn err_fn(_: cpal::StreamError) {
-    println!("REEEEEEE")
 }
